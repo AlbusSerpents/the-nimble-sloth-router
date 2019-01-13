ecfce73c30f0fb495c8db5505f05ca00eae72bb9
AlbusSerpents <naidim@abv.bg>
2019-01-13 17:35:23 +0200

    fixed a problem with apps' urls for the liveliness check

--- src/main/java/com/nimble/sloth/router/func/liveliness/LivelinessService.java
+++ src/main/java/com/nimble/sloth/router/func/liveliness/LivelinessService.java
@@ -3,6 +3,8 @@ package com.nimble.sloth.router.func.liveliness;
 import com.nimble.sloth.router.func.liveliness.AppLiveliness.LivelinessStatus;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,11 +12,13 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 import static com.nimble.sloth.router.func.liveliness.AppLiveliness.LivelinessStatus.*;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @SuppressWarnings("WeakerAccess")
 @Service
 public class LivelinessService {
-    private static final String STATUS_SUFFIX = "/status";
+    private static final String STATUS_SUFFIX = "status";
     private static final Log LOG = LogFactory.getLog(LivelinessService.class);
 
     private final LivelinessRepository repository;
@@ -39,13 +43,20 @@ public class LivelinessService {
     }
 
     private String makeUrl(final LivelinessRequest request) {
-        return request.getUrl() + STATUS_SUFFIX;
+        final String url = request.getUrl();
+        return url.endsWith("/") ?
+                String.format("%s%s", url, STATUS_SUFFIX) :
+                String.format("%s/%s", url, STATUS_SUFFIX);
     }
 
     private LivelinessStatus callServer(final String fullUrl) {
         final RestTemplate restTemplate = new RestTemplate();
         try {
-            final ResponseEntity<Object> result = restTemplate.getForEntity(fullUrl, Object.class);
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(APPLICATION_JSON);
+            final HttpEntity<Object> entity = new HttpEntity<>(null, headers);
+
+            final ResponseEntity<Object> result = restTemplate.exchange(fullUrl, GET, entity, Object.class);
             return result.getStatusCode().is2xxSuccessful() ? OK : DOWN;
         } catch (Exception e) {
             LOG.error(e);
